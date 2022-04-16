e2e38c91dc609d0d3a907a025402a5aef8928fe2
clbrunet <brunet.clement5@gmail.com>
2022-04-16 17:48:03 +0200

    feat: add empty Quiz component and display it when the quiz starts

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { useState } from 'react';
 
 import './App.css';
 import Home from './components/Home'
+import Quiz from './components/Quiz'
 
 function App() {
   const [isQuizStarted, setIsQuizStarted] = useState(false);
 
   return (
     <div className="App">
-      {isQuizStarted === false && <Home startQuiz={() => setIsQuizStarted(true)} />}
+      { isQuizStarted === false
+        ? <Home startQuiz={() => setIsQuizStarted(true)} />
+        : <Quiz /> }
     </div>
   );
 }
---
+++ src/components/Quiz.tsx
@@ -0,0 +1,11 @@
+import './Quiz.css';
+
+function Quiz() {
+  return (
+    <div className="Quiz">
+      <p>Quiz</p>
+    </div>
+  );
+}
+
+export default Quiz;
